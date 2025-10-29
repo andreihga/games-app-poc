0bd3f7755011bd0c34d57af2a783c837127780a5
Andrei Hogea <andreihga@gmail.com>
2025-10-29 23:43:41 +0200

    test connection to VPS

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/api/hello")
 def hello():
-    return jsonify(message="Hello Ade!")
+    return jsonify(message="Hello with changes from VPS!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
