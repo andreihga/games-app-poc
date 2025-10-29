42eff346f8358a20a6f2548d791d109f06af3433
Andrei Hogea <andreihga@gmail.com>
2025-10-29 17:22:05 +0200

    Update to V3

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/api/hello")
 def hello():
-    return jsonify(message="Hello from Flask backend V2.0!")
+    return jsonify(message="Hello from Flask backend V3.0!")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
